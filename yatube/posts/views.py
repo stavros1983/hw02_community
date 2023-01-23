3740681c886fd4eee402912ea6b111b65002991c
Бабаян Армен <stavros1983@yandex.ru>
2023-01-23 06:42:10 +0400

    after 2 review minus comments

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,29 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-# Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
-# Create your views here.
 
 LAST_PUB = 10
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию
     posts = Post.objects.all()[:LAST_PUB]
-    # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
 
-# View-функция для страницы сообщества:
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:LAST_PUB]
     context = {
